d240c9eab80fc3aac14c49fc30e1f76ec79d74b1
bakarydrame <bakisdu95@hotmail.fr>
2022-06-16 15:30:14 +0200

    :memo: Add student migration :memo:

---
+++ database/migrations/2022_06_16_130031_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('lastName');
+            $table->string('firstName');
+            $table->date('age');
+            $table->string('email');
+            $table->string('studie');
+            $table->string('phone');
+            $table->string('educationLevel');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
