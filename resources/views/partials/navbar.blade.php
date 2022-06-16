cf0794463d84eb5527741787f25a2374848d12f3
bakarydrame <bakisdu95@hotmail.fr>
2022-06-16 15:29:34 +0200

    :memo: Add navbar template :memo:

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,5 @@
+<u>
+    <li><a href="{{ route('welcome') }}">L'accueil</a></li>
+    <li><a href="{{ route('upgrade.student') }}">Créer un nouveau post</a></li>
+    <li><a href="{{ route('contact') }}">Page de contact</a></li>
+</u>
