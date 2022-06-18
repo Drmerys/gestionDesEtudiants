938ffdb6cb3776247e3607d6d2412b7bde8431c2
bakarydrame <bakisdu95@hotmail.fr>
2022-06-18 20:15:10 +0200

    :construction: Implement showUpgrade and addUpgrade :construction:

--- app/Http/Controllers/UpgradeController.php
+++ app/Http/Controllers/UpgradeController.php
@@ -2,11 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Upgrade;
+
 class UpgradeController extends Controller
 {
     public function showUpgrade()
     {
         $title = "Les notes !";
-        return view('student.upgrade', compact('title'));
+        $getUpgrades = Upgrade::all();
+        $this->addUpgrade();
+
+        return view('student.upgrade', [
+            'title' => $title,
+            'upgrades' => $getUpgrades
+        ] );
+    }
+
+    public function addUpgrade()
+    {
+          $matter =  Upgrade::create([
+              'matter' => 'Observer', 'upgrade' => 15 ,'date' => now(), 'average' => 15
+          ]);
+
+        $matter->save();
+    }
+
+    public function deleteUpgrade()
+    {
+
+    }
+
+    public function updateUpgrade()
+    {
+
     }
 }
