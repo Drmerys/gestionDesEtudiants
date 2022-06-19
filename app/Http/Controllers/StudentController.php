1e7267445beb9e811efbb1d05e5aaa5e027e8db5
bakarydrame <bakisdu95@hotmail.fr>
2022-06-20 00:20:07 +0200

    :construction: Implemente store method :construction:

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -19,10 +19,39 @@ class StudentController extends Controller
         return view('student.student', compact('students'));
     }
 
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function store(Request $request)
+    {
+        $validateData = $request->validate([
+            'lastName' => 'required|max:255',
+            'firstName' => 'required|max:255',
+            'age' => 'required',
+            'email' => 'required|max:255',
+            'educationSector' => 'required|max:255',
+            'phone' => 'required',
+            'educationLevel' => 'required|max:255',
+            'image' => 'required|image|mimes:png,jpeg,jpg,gif,svg',
+        ]);
+
+        $image = $request->file('image');
+        $destinationPath = 'image/';
+        $profileImage = date('YmdHis'). "." .$image->getClientOriginalExtension();
+        $image->move($destinationPath, $profileImage);
+        $validateData['image'] = $profileImage;
+
+        $students = Student::create($validateData);
+
+        return redirect('students.students');
+    }
+
     public function show()
     {
-        $students = Student::all();
-        return view('student.student', compact('students'));
+//        $students = Student::all();
+//        return view('student.student', compact('students'));
     }
 
     public function contact()
