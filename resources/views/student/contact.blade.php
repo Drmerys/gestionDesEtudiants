e51556eedee0914d4a64c92b53cf0468caaf7365
bakarydrame <bakisdu95@hotmail.fr>
2022-06-23 00:49:25 +0200

    :construction:  Add contact template [WIP] :construction:

---
+++ resources/views/student/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="text-center mt-2">
+        <p>
+            mail :univ_paris13@univ.net
+        </p>
+
+        <p>
+            Tel : 0145984482
+        </p>
+
+    </div>
+@endsection
