9ec928fcfbc7f5031ecabfe923d485632d58b2ac
bakarydrame <bakisdu95@hotmail.fr>
2022-06-23 14:10:09 +0200

    :memo: Add Annotation :memo:

--- app/Http/Controllers/UpgradeController.php
+++ app/Http/Controllers/UpgradeController.php
@@ -12,19 +12,32 @@ class UpgradeController extends Controller
         $getUpgrades = Upgrade::all();
         //$this->addUpgrade();
 
-        return view('student.upgrade', [
+        return view('upgrade.upgrade', [
             'title' => $title,
             'upgrades' => $getUpgrades
         ] );
     }
 
-    public function addUpgrade()
+    /**
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function create()
     {
-          $matter =  Upgrade::create([
-              'matter' => 'Observer', 'upgrade' => 15 ,'date' => now(), 'average' => 15
-          ]);
+          return view('upgrade.create');
+    }
 
-        $matter->save();
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(Request $request)
+    {
+//          $matter =  Upgrade::create([
+//              'matter' => 'Observer', 'upgrade' => 15 ,'date' => now(), 'average' => 15
+//          ]);
+//
+//        $matter->save();
+        return redirect('upgrade')->with('success', 'Etudiant ajouté avec succès');
     }
 
     public function deleteUpgrade()
